8aa3df8facfdac11af6d12241a0ac873ef6884c6
Lolpawer <dadu.petrosino@gmail.com>
2021-12-02 16:45:18 +0100

    password generator

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@
 //prompt del colore (const)
 //poi faccio un nome + cognome + colore + 21
 //poi stampo il risultato
+
+//prompt
+const inputNome = prompt('Inserisci il tuo nome');
+const inputCognome = prompt('Inserisci il tuo cognome');
+const inputColore = prompt('Inserisci il tuo colore preferito');
+
+//creazione password
+let createdPassword = inputNome + inputCognome + inputColore + 21 ;
+
+//stampa password
+let outputPassword = document.getElementById('password');
+outputPassword.innerHTML = `La tua password è ${createdPassword}`;
